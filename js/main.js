cf7423b5214c7c2ae6805bbc71427e52c314e297
MrLuto <mmudde2@gmail.com>
2022-12-24 16:36:12 +0100

    update

--- js/main.js
+++ js/main.js
@@ -74,7 +74,7 @@ function includeHTML() {
     file = getParameterByName('q');
     if (file) {
       /* Make an HTTP request using the attribute value as the file name: */
-      fetch("/pages/" + file)
+      fetch(file)
       .then(response => response.text())
       .then(data => elmnt.innerHTML = data);
 
