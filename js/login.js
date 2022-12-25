9a04061d7a0360abf3459f7c6a983b902362e4d2
MrLuto <mmudde2@gmail.com>
2022-12-25 21:16:50 +0100

    yes

--- js/login.js
+++ js/login.js
@@ -3,6 +3,7 @@ function login() {
         alert("All details are required");
         return;
     }
+    
     document.cookie = "uid=" + document.getElementById('email').value + "; path=/; max-age="+60*60*24*30; 
 }
 
@@ -17,5 +18,22 @@ function Register() {
         alert("The passwords do not match");
         return;
     }
+    // add user to cookie
     document.cookie = "uid=" + document.getElementById('email').value + "; path=/; max-age="+60*60*24*30; 
+    // add user to ./db/users.json
+    var user = {
+        "email": document.getElementById('email').value,
+        "password": document.getElementById('password').value,
+        "firstname": document.getElementById('firstname').value,
+        "lastname": document.getElementById('lastname').value
+    };
+    var users = JSON.parse("./db/users.json");
+    users.push(user);
+    var json = JSON.stringify(users);
+    fs.writeFile("./db/users.json", json, 'utf8', function(err) {
+        if (err) {
+            console.log(err);
+        }
+    });
+
 }
