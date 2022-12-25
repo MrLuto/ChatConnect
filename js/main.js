c1c86ad72afb852007bfb84766a0d67b89f7176a
MrLuto <mmudde2@gmail.com>
2022-12-25 21:03:42 +0100

    fixed a leak

--- js/main.js
+++ js/main.js
@@ -86,7 +86,6 @@ function includeHTML() {
 
 // make a function that checks if the user is logged in
 function checkLogin() {
-  console.log(document.cookie);
   //check if parameter equals ?q=login.html
   if (getParameterByName('q') === "login.html") return;
   if (!document.cookie.startsWith('uid=')) {
