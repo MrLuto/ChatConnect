36c494413960ccfa84776f37b0fdc10454bf4f9d
MrLuto <mmudde2@gmail.com>
2022-12-25 22:05:58 +0100

    yes

--- js/login.js
+++ js/login.js
@@ -4,20 +4,21 @@ function login() {
         return;
     }
     // send data to https://node25.mc-node.net:26133/ChatConnect/login and get response
-    fetch("https://example.com/", {
-        method: "POST",
-        body: JSON.stringify({
-            email: document.getElementById('email').value,
-            password: document.getElementById('password').value
-        })
-    }).then(response => response.json()).then(data => {
-        if (data.success) {
-            document.cookie = "uid=" + document.getElementById('email').value + "; path=/; max-age="+60*60*24*30; 
-            window.location.href = "/index.html?q=dashboard.html";
-        } else {
-            alert("something went wrong");
-        }
-    });
+    //fetch("https://node25.mc-node.net:26133/ChatConnect/login", {
+    //    method: "POST",
+    //    body: JSON.stringify({
+    //        email: document.getElementById('email').value,
+    //        password: document.getElementById('password').value
+    //    })
+    //}).then(response => response.json()).then(data => {
+    //    if (data.success) {
+    //        document.cookie = "uid=" + document.getElementById('email').value + "; path=/; max-age="+60*60*24*30; 
+    //        window.location.href = "/index.html?q=dashboard.html";
+    //    } else {
+    //        alert("something went wrong");
+    //    }
+    //});
+    window.location.href = "/index.html?q=dashboard.html";
 }
 
 function Register() {
@@ -32,20 +33,21 @@ function Register() {
         return;
     }
     // send data to https://node25.mc-node.net:26133/ChatConnect/register and get response
-    fetch("https://example.com/", {
-        method: "POST",
-        body: JSON.stringify({
-            email: document.getElementById('email').value,
-            password: document.getElementById('password').value,
-            firstname: document.getElementById('firstname').value,
-            lastname: document.getElementById('lastname').value
-        })
-    }).then(response => response.json()).then(data => {
-        if (data.success) {
-            document.cookie = "uid=" + document.getElementById('email').value + "; path=/; max-age="+60*60*24*30;
-            window.location.href = "/index.html?q=dashboard.html";
-        } else {
-            alert("something went wrong");
-        }
-    });
+    //fetch("https://node25.mc-node.net:26133/ChatConnect/register", {
+    //    method: "POST",
+    //    body: JSON.stringify({
+    //        email: document.getElementById('email').value,
+    //        password: document.getElementById('password').value,
+    //        firstname: document.getElementById('firstname').value,
+    //        lastname: document.getElementById('lastname').value
+    //    })
+    //}).then(response => response.json()).then(data => {
+    //    if (data.success) {
+    //        document.cookie = "uid=" + document.getElementById('email').value + "; path=/; max-age="+60*60*24*30;
+    //        window.location.href = "/index.html?q=dashboard.html";
+    //    } else {
+    //        alert("something went wrong");
+    //    }
+    //});
+    window.location.href = "/index.html?q=dashboard.html";
 }
