58e1919c3f34820c2d236aae8aefba2bef65a3a7
MrLuto <mmudde2@gmail.com>
2022-12-26 11:27:32 +0100

    ys

--- js/login.js
+++ js/login.js
@@ -6,7 +6,7 @@ function login() {
     // send data to https://node25.mc-node.net:26133/ChatConnect/login and get response
     fetch("http://node25.mc-node.net:26133/ChatConnect/login?email=" + document.getElementById('email').value + "&password=" + document.getElementById('password').value).then(response => response.json()).then(data => {
         console.log(data);
-        if (data.success) {
+        if (response.status == 200) {
             document.cookie = "uid=" + document.getElementById('email').value + "; path=/; max-age="+60*60*24*30;
             window.location.href = "/index.html?q=dashboard.html";
         } else {
@@ -29,7 +29,7 @@ function Register() {
     // send data to https://node25.mc-node.net:26133/ChatConnect/register and get response
     // #TODO
     fetch("http://node25.mc-node.net:26133/ChatConnect/register?email=" + document.getElementById('email').value + "&password=" + document.getElementById('password').value + "&firstname=" + document.getElementById('firstname').value + "&lastname=" + document.getElementById('lastname').value).then(response => response.json()).then(data => {
-        if (data.success) {
+        if (response.status == 200) {
             document.cookie = "uid=" + document.getElementById('email').value + "; path=/; max-age="+60*60*24*30;
             window.location.href = "/index.html?q=dashboard.html";
         } else {
