eb365db41b850e8a600b7498488beca184750851
MrLuto <mmudde2@gmail.com>
2022-12-26 10:43:53 +0100

    get requests are better

--- js/login.js
+++ js/login.js
@@ -36,15 +36,7 @@ function Register() {
     }
     // send data to https://node25.mc-node.net:26133/ChatConnect/register and get response
     // #TODO
-    fetch("https://node25.mc-node.net:26133/ChatConnect/register", {
-        method: "POST",
-        body: JSON.stringify({
-            email: document.getElementById('email').value,
-            password: document.getElementById('password').value,
-            firstname: document.getElementById('firstname').value,
-            lastname: document.getElementById('lastname').value
-        })
-    }).then(response => response.json()).then(data => {
+    fetch("https://node25.mc-node.net:26133/ChatConnect/register?email=" + document.getElementById('email').value + "&password=" + document.getElementById('password').value + "&firstname=" + document.getElementById('firstname').value + "&lastname=" + document.getElementById('lastname').value).then(response => response.json()).then(data => {
         if (data.success) {
             document.cookie = "uid=" + document.getElementById('email').value + "; path=/; max-age="+60*60*24*30;
             window.location.href = "/index.html?q=dashboard.html";
@@ -52,6 +44,4 @@ function Register() {
             alert("something went wrong");
         }
     });
-    cument.cookie = "uid=" + document.getElementById('email').value + "; path=/; max-age="+60*60*24*30;
-    window.location.href = "/index.html?q=dashboard.html";
 }
